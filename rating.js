66dca9f425ecf219df7be457aec8529419e29743
Roger Mateu <59694834+rmateusemis@users.noreply.github.com>
2020-05-11 18:43:49 +0200

    Update rating.js

--- rating.js
+++ rating.js
@@ -4,22 +4,38 @@ const stars = document.querySelector('.ratings').children;
 //Recorre cada elemento de stars y añade evento
 for (let i = 0; i < stars.length; i++) {
     stars[i].addEventListener('mouseover', function(){
-
-        // Recorre stars de nuevo y cambia la clase cuando esta rellena (efecto inverso)
-        for (let j = 0; j < stars.length; j++) {
-            stars[j].classList.remove("fa-star");
-            stars[j].classList.add("fa-star-o"); 
-        }
-       
-        // Cambia clase añadiendo estrella rellena de 0 a [i] incluido este
-        for (let j = 0; j <= i; j++) {
-            stars[j].classList.remove("fa-star-o");
-            stars[j].classList.add("fa-star");
-        }
+		set_starts(i);
     });
     stars[i].addEventListener('click', function(){
         console.log(stars[i]);
+		set_starts(i);
     
-    });
-    
+	});
+	// Resetea al hacer click fuera las estrellas
+	document.addEventListener('click', function(e) {
+		var container = document.querySelector('.ratings');
+		if (!container.contains(e.target)) {
+		  stars[i].classList.remove("fa-star");
+		  stars[i].classList.add("fa-star-o");
+		}
+	  });
+}
+
+
+function set_starts(i){
+	console.log("set_starts"+i);
+	clean_all_stars();
+	for (let j = 0; j <= i; j++) {
+		stars[j].classList.remove("fa-star-o");
+		stars[j].classList.add("fa-star"); 
+	}
+}
+
+function clean_all_stars(){
+	console.log("clean_all_stars");
+	for (let j = 0; j < stars.length; j++) {
+		stars[j].classList.remove("fa-star");
+		stars[j].classList.add("fa-star-o");
+
+	}
 }
